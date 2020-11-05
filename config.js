1b4658be76166d06c33495b3a820df96f6c314ff
Turkay Tunc <turkaytunc00@gmail.com>
2020-11-05 15:48:10 +0300

    add post schema

--- config.js
+++ config.js
@@ -2,4 +2,5 @@ require('dotenv').config();
 
 module.exports = {
   mongoConnectionString: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@codecamp-graphql.ihj2d.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`,
+  PORT: 4000,
 };
--- index.js
+++ index.js
@@ -2,17 +2,34 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const { mongoConnectionString } = require('./config');
+const { mongoConnectionString, PORT } = require('./config');
+
+const Post = require('./models/Post');
+const User = require('./models/User');
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => 'Hello from Apollo',
+    getPosts: async () => {
+      try {
+        const posts = await Post.find();
+
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
   },
 };
 
@@ -27,5 +44,5 @@ mongoose
     useNewUrlParser: true,
   })
   .then(() => console.log('Connected to mongo atlas'))
-  .then(() => server.listen({ port: 4000 }))
+  .then(() => server.listen(PORT))
   .then((res) => console.log(`Server running at ${res.url}`));
