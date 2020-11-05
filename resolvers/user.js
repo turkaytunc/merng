74798537d182de5a510ba05c7a84ad913d49892a
Turkay Tunc <turkaytunc00@gmail.com>
2020-11-05 17:32:39 +0300

    add graphql mutations

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,4 +1,8 @@
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/User');
+const SECRET = process.env.SECRET;
 
 module.exports = {
   Query: {
@@ -9,6 +13,35 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
+    },
+  },
+  Mutation: {
+    register: async (parent, args, context, info) => {
+      const { username, email, password, confirmPassword } = args.registerInput;
+      const passwordHash = await bcryptjs.hash(password, 12);
+      const newUser = new User({
+        email,
+        username,
+        password: passwordHash,
+        createdAt: new Date().toISOString(),
+      });
+
+      const response = await newUser.save();
+      const token = jwt.sign(
+        {
+          id: response.id,
+          email: response.email,
+          username: response.username,
+        },
+        SECRET,
+        { expiresIn: '1h' }
+      );
+
+      return {
+        ...response._doc,
+        id: response._id,
+        token,
+      };
     },
   },
 };
--- typedefs/typeDefs.js
+++ typedefs/typeDefs.js
@@ -7,8 +7,27 @@ const typeDefs = gql`
     createdAt: String!
     username: String!
   }
+  type User {
+    id: ID!
+    email: String!
+    token: String!
+    username: String!
+    createdAt: String!
+  }
+
+  input RegisterInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
+
   type Query {
     getPosts: [Post]
+    getUsers: [User]
+  }
+  type Mutation {
+    register(registerInput: RegisterInput): User!
   }
 `;
 
